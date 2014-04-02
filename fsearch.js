34ca152f9dc0cfc582e4a880254071011835a660
hsientsungwu <hwu1986@gmail.com>
2014-04-01 23:48:23 -0400

    plugin rewrite

--- fsearch.js
+++ fsearch.js
@@ -3,67 +3,62 @@
  * @author HsienTsung Wu
  */
 
-(function ( $ ) {
+function Search( options ) {
+	var _this = this;
 
-	$.fn.fsearch = function ( options ) {
-		var _this = this;
+	var defaults = {
+		input 			: '#search-field',
+		length 			: 2,
+		list 			: '.search-list',
+		target 			: 'data-content',
+		keyupCallback 	: function() {},
+		resetCallback 	: function() {},
+		searchCallback 	: function(count) {}
+	};
 
-		var defaults = {
-			length 			: 2,
-			list 			: '.search-list',
-			target 			: 'data-content',
-			keyupCallback 	: function() {},
-			resetCallback 	: function() {},
-			searchCallback 	: function(count) {}
-		};
+	_this.settings = $.extend(defaults, options);
 
-		var settings = $.extend(defaults, options);
+	this.search = function(key) {
+    	var count = 0;
 
-		function search(key) {
-	    	var count = 0;
+    	$(_this.settings.list).each(function() { 
+            if ($(this).attr(_this.settings.target).length === undefined) {
+                $(this).hide();
+            } else if ($(this).attr(_this.settings.target).search(new RegExp(key, "i")) < 0) {
+                $(this).hide();
+            } else {
+                $(this).show();
+                count++;
+            }
+        });
 
-	    	$(settings.list).each(function() { 
-	            if ($(this).attr(settings.target).length === undefined) {
-	                $(this).hide();
-	            } else if ($(this).attr(settings.target).search(new RegExp(key, "i")) < 0) {
-	                $(this).hide();
-	            } else {
-	                $(this).show();
-	                count++;
-	            }
-	        });
-
-	        if (typeof settings.searchCallback == 'function') settings.searchCallback(count);
-		}
-
-		function displayAll() {
-			$(settings.list).each(function(e) {
-				$(this).show();
-			});
-		}
-
-		function reset() {
-			if (typeof settings.resetCallback == 'function') settings.resetCallback();
-
-			displayAll();
-			$(this).val('');
-		}
-
-		function init () {
-			$(_this).keyup(function(e) { console.log('keyup');
-				if (typeof settings.keyupCallback == 'function') settings.keyupCallback();
-
-				var key = $(this).val();
-
-				if (key.length >= settings.length) {
-					search(key);
-				} else {
-					displayAll();
-				}
-			});
-		};
-
-		return init();
+        if (typeof _this.settings.searchCallback == 'function') _this.settings.searchCallback(count);
 	}
 
-} ( jQuery ));
+	this.displayAll = function() {
+		$(_this.settings.list).each(function(e) {
+			$(this).show();
+		});
+	}
+
+	this.reset = function() {
+		if (typeof _this.settings.resetCallback == 'function') _this.settings.resetCallback();
+
+		_this.displayAll();
+		$(_this.settings.input).val('');
+	};
+
+	$(_this.settings.input).keyup(function(e) {
+		if (typeof _this.settings.keyupCallback == 'function') _this.settings.keyupCallback();
+
+		var key = $(this).val();
+
+		if (key.length >= _this.settings.length) {
+			_this.search(key);
+		} else {
+			_this.displayAll();
+		}
+	});
+
+	return this;
+}
